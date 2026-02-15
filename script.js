ac049af9512a35157dcb2aace441105a9c9906e5
AnvarbekToshmamatov <sizning_github_emailingiz@gmail.com>
2026-02-15 15:46:23 +0500

    ic3

--- script.js
+++ script.js
@@ -730,6 +730,7 @@ const categories = [
   { id: "Printing", name: "Chop etish", icon: "🖨️" },
   { id: "Communication", name: "Muloqot", icon: "💬" },
   { id: "Health", name: "Salomatlik", icon: "❤️" },
+  { id: "Kurs", name: "IC3 GS6 kursi", icon: "📒" },
 ];
 
 // Har bir kategoriyadagi atamalar sonini hisoblash
@@ -993,38 +994,69 @@ function renderCategories() {
   categoryContainer.innerHTML = "";
 
   categories.forEach((cat) => {
-    const card = document.createElement("div");
-    card.className = "category-card";
-    card.innerHTML = `
+    if (cat.id != "Kurs") {
+      const card = document.createElement("div");
+      card.className = "category-card";
+      card.innerHTML = `
             <div class="category-icon">${cat.icon}</div>
             <span class="category-name">${cat.name}</span>
             <span class="category-count">${cat.count} ta atama</span>
         `;
 
-    card.addEventListener("click", () => loadCategoryTest(cat.id));
-    categoryContainer.appendChild(card);
+      card.addEventListener("click", () => loadCategoryTest(cat.id));
+      categoryContainer.appendChild(card);
+    } else {
+      const card = document.createElement("div");
+      card.className = "category-card";
+      card.innerHTML = `
+            <div class="category-icon">${cat.icon}</div>
+            <span class="category-name">${cat.name}</span>
+            <a href="https://t.me/Informatika_ic3_ser" class="tg_link">
+              <button class="tg_kanal">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-telegram"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.287 5.906q-1.168.486-4.666 2.01-.567.225-.595.442c-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294q.39.01.868-.32 3.269-2.206 3.374-2.23c.05-.012.12-.026.166.016s.042.12.037.141c-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8 8 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629q.14.092.27.187c.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.4 1.4 0 0 0-.013-.315.34.34 0 0 0-.114-.217.53.53 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09"
+                  />
+                </svg>
+                Digital Ustoz | Informatika
+              </button></a
+            >
+        `;
+
+      card.addEventListener("click", () => loadCategoryTest(cat.id));
+      categoryContainer.appendChild(card);
+    }
   });
 }
 
 // -------------------- TESTNI YUKLASH --------------------
 function loadCategoryTest(categoryId) {
-  currentCategory = categories.find((c) => c.id === categoryId);
+  if (categoryId != "Kurs") {
+    currentCategory = categories.find((c) => c.id === categoryId);
 
-  // Kategoriyadagi atamalarni olish
-  const categoryTerms = fullGlossary.filter((t) => t.category === categoryId);
+    // Kategoriyadagi atamalarni olish
+    const categoryTerms = fullGlossary.filter((t) => t.category === categoryId);
 
-  // 10----------------- ta savol generatsiya qilish
-  currentQuestions = generateQuestions(categoryTerms, categoryTerms.length);
+    // 10----------------- ta savol generatsiya qilish
+    currentQuestions = generateQuestions(categoryTerms, categoryTerms.length);
 
-  currentIndex = 0;
-  userAnswers = new Array(60).fill(null);
-  score = 0;
+    currentIndex = 0;
+    userAnswers = new Array(60).fill(null);
+    score = 0;
 
-  mainSection.style.display = "none";
-  testSection.style.display = "block";
-  testCategoryTitle.innerHTML = `${currentCategory.icon} ${currentCategory.name}`;
+    mainSection.style.display = "none";
+    testSection.style.display = "block";
+    testCategoryTitle.innerHTML = `${currentCategory.icon} ${currentCategory.name}`;
 
-  renderQuestion();
+    renderQuestion();
+  }
 }
 
 // -------------------- SAVOLLARNI GENERATSIYA QILISH --------------------
